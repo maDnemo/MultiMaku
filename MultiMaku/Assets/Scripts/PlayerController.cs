c34e2959b129f81697f9dd379d2cf1dacecb02ff
Austin Cheng <austinbicheng@gmail.com>
2017-09-05 15:57:34 -0700

    a little networking and arrows :D

---
+++ MultiMaku/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController: NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, 0, -x);
+        transform.Translate(0, z, 0);
+    }
+}
