d68bf1a5e561204c8be8da05633e7340e93d045d
Austin Cheng <austinbicheng@gmail.com>
2017-09-06 11:07:38 -0700

    shoot blueshells at toher players

--- MultiMaku/Assets/Scripts/PlayerController.cs
+++ MultiMaku/Assets/Scripts/PlayerController.cs
@@ -27,11 +27,12 @@ public class PlayerController: NetworkBehaviour {
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Fire();
+            CmdFire();
         }
     }
 
-    void Fire()
+    [Command]
+    void CmdFire()
     {
         // Create the Bullet from the Bullet Prefab
         var bullet = (GameObject)Instantiate(
@@ -41,6 +42,8 @@ public class PlayerController: NetworkBehaviour {
 
         // Add velocity to the bullet
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 6;
+
+        NetworkServer.Spawn(bullet);
 
         // Destroy the bullet after 2 seconds
         Destroy(bullet, 2.0f);
