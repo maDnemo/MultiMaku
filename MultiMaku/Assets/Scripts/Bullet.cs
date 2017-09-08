44c2a6ca09037dd59ecec5609546854e24cdc187
Austin Cheng <austinbicheng@gmail.com>
2017-09-07 19:21:43 -0700

    hp bars, cannot seem to get collisions to work

---
+++ MultiMaku/Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(1);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ MultiMaku/Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public const int maxHealth = 10;
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+	
+    public void TakeDamage(int amount)
+    {
+        Debug.Log("player has been hit");
+        currentHealth = currentHealth - amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("deaded!");
+        }
+
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+}
