a0998959b21a1170617409bf3547bfdf2989bec6
SACCFFT <reck98@gmail.com>
2017-09-16 19:36:30 -0400

    Baisc editing

--- MultiMaku/Assets/Scripts/PlayerController.cs
+++ MultiMaku/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerController: NetworkBehaviour {
-    public double rateOfPrimaryFire = 0.3;
+    
+
+	public double rateOfPrimaryFire = 0.3;
     public double rateOfSecondaryFire = 3.0;
     private double nextPrimaryFire;
     private double nextSecondaryFire;
@@ -23,20 +25,9 @@ public class PlayerController: NetworkBehaviour {
             return;
         }
 
-        //var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+		Move ();
 
-        if (Input.GetButton("Fire3"))
-        {
-            x = x * (float)0.5;
-            z = z * (float)0.5;
-        }
-
-        //transform.Rotate(0, 0, -x);
-        transform.Translate(x, 0, 0);
-        transform.Translate(0, z, 0);
-
+        
         if (Input.GetButton("Fire1") && Time.time > nextPrimaryFire)
         {
             nextPrimaryFire = Time.time + rateOfPrimaryFire;
@@ -49,6 +40,26 @@ public class PlayerController: NetworkBehaviour {
             CmdSecondFire();
         }
     }
+
+
+	// Move and rotate character
+	[Command]
+	void Move()
+	{
+		//var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		if (Input.GetButton("Fire3"))
+		{
+			x = x * (float)0.5;
+			z = z * (float)0.5;
+		}
+
+		//transform.Rotate(0, 0, -x);
+		transform.Translate(x, 0, 0);
+		transform.Translate(0, z, 0);
+	}
 
     [Command]
     void CmdFire()
