0719ce460cd99297640db4a5ef7028caf1cd2fad
Austin Cheng <austinbicheng@gmail.com>
2017-09-15 17:18:50 -0700

    secondary shot added (c). need to find out how to unbind X

--- MultiMaku/Assets/Scripts/Health.cs
+++ MultiMaku/Assets/Scripts/Health.cs
@@ -13,11 +13,6 @@ public class Health : NetworkBehaviour {
 	
     public void TakeDamage(int amount)
     {
-        if (!isServer)
-        {
-            return;
-        }
-
         Debug.Log("player has been hit");
         currentHealth = currentHealth - amount;
         if (currentHealth <= 0)
--- MultiMaku/Assets/Scripts/PlayerController.cs
+++ MultiMaku/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class PlayerController: NetworkBehaviour {
-    public double rateOfFire = 0.3;
-    private double nextFire;
+    public double rateOfPrimaryFire = 0.3;
+    public double rateOfSecondaryFire = 3.0;
+    private double nextPrimaryFire;
+    private double nextSecondaryFire;
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
 	// Use this for initialization
 	void Start () {
-        nextFire = Time.time;
+        nextPrimaryFire = Time.time;
+        nextSecondaryFire = Time.time;
     }
 
     // Update is called once per frame
@@ -26,15 +29,38 @@ public class PlayerController: NetworkBehaviour {
         transform.Rotate(0, 0, -x);
         transform.Translate(0, z, 0);
 
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        if (Input.GetButton("Fire1") && Time.time > nextPrimaryFire)
         {
-            nextFire = Time.time + rateOfFire;
+            nextPrimaryFire = Time.time + rateOfPrimaryFire;
             CmdFire();
+        }
+
+        if (Input.GetButton("Fire2") && Time.time > nextSecondaryFire)
+        {
+            nextSecondaryFire = Time.time + rateOfSecondaryFire;
+            CmdSecondFire();
         }
     }
 
     [Command]
     void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.up * 6;
+
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+    [Command]
+    void CmdSecondFire()
     {
         // Create the Bullet from the Bullet Prefab
         var bullet = (GameObject)Instantiate(
