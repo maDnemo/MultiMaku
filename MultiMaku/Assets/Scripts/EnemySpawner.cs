e77bc3cf103a13dfce3ad142b53bf074627df0a8
Austin Cheng <austinbicheng@gmail.com>
2017-09-19 15:57:30 -0700

    added some basic spawn patterns, its now a shoot em up lamo

--- MultiMaku/Assets/Scripts/EnemySpawner.cs
+++ MultiMaku/Assets/Scripts/EnemySpawner.cs
@@ -5,15 +5,39 @@ public class EnemySpawner : NetworkBehaviour
 {
 
     public GameObject enemyPrefab;
-    public int numberOfEnemies;
+    public int numberOfEnemiesOnSpawn;
+    public int maxNumberOfEnemies;
+    public double enemySpawnDelay;
+    public double nextEnemySpawn;
 
     public override void OnStartServer()
     {
-        for (int i = 0; i < numberOfEnemies; i++)
+        nextEnemySpawn = Time.time;
+        for (int i = 0; i < numberOfEnemiesOnSpawn; i++)
         {
             var spawnPosition = new Vector3(
-                Random.Range(-5.0f, 5.0f),
-                Random.Range(-5.0f, 5.0f),
+                Random.Range(-3.5f, 3.5f),
+                Random.Range(0f, 5.0f),
+                0.0f);
+
+            var spawnRotation = Quaternion.Euler(
+                0.0f,
+                0.0f,
+                0.0f);
+
+            var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation);
+            NetworkServer.Spawn(enemy);
+        }
+    }
+
+    public void Update()
+    {
+        if (Time.time > nextEnemySpawn)
+        {
+            nextEnemySpawn = Time.time + enemySpawnDelay;
+            var spawnPosition = new Vector3(
+                Random.Range(-3.5f, 3.5f),
+                Random.Range(0f, 5.0f),
                 0.0f);
 
             var spawnRotation = Quaternion.Euler(
